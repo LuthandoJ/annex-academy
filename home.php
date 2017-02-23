ba71a9fbc86dbf079aaffcf06a35777dde902adf
asithembiso <asithembiso@gmail.com>
2017-02-23 20:32:47 +0200

    cleaner look

--- home.php
+++ home.php
@@ -48,19 +48,10 @@
                     <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
                     <ul class="dropdown-menu">
-                        <li>
-                            <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-fw fa-envelope"></i> Inbox</a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-fw fa-gear"></i> Settings</a>
-                        </li>
+                        <li><a href="profile.php"><i class="fa fa-fw fa-user"></i> Profile</a></li>
+                        <li><a href="settings.php"><i class="fa fa-fw fa-gear"></i> Settings</a></li>
                         <li class="divider"></li>
-                        <li>
-                            <a href="logout.php"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
-                        </li>
+                        <li><a href="logout.php"><i class="fa fa-fw fa-power-off"></i> Log Out</a></li>
                     </ul>
                 </li>
                 </ul>
@@ -69,7 +60,7 @@
         </div>
         <!-- /.container -->
     </nav>
-    
+
 
     <!-- Page Content -->
     
