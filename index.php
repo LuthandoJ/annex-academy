212b25501769d0b2f36de2afa9260212fc5fcc1f
asithembiso <asithembiso@gmail.com>
2017-02-28 13:45:38 +0200

    tutorial links

--- index.php
+++ index.php
@@ -115,8 +115,55 @@
                   </div>
               </div>
           </section>
+          <br>
+          <center><h2 class="section-heading"><i class="fa fa-file-video-o" aria-hidden="true"></i> Turorials</h2></center>
+          <br>
+          <div class="container">
+          <div class="row">
+            <div class="col-xs-18 col-sm-6 col-md-3">
+              <div class="thumbnail">
+                <img src="http://placehold.it/500x250/EEE">
+                  <div class="caption">
+                    <h4>Thumbnail label</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, soluta, eligendi doloribus sunt minus amet sit debitis repellat. Consectetur, culpa itaque odio similique suscipit</p>
+                    <a href="#" class="btn btn-default btn-xs pull-right" role="button"><i class="glyphicon glyphicon-edit"></i></a> <a href="#" class="btn btn-info btn-xs" role="button">Button</a> <a href="#" class="btn btn-default btn-xs" role="button">Button</a>
+                </div>
+              </div>
+            </div>
 
-          <hr class="primary">
+            <div class="col-xs-18 col-sm-6 col-md-3">
+              <div class="thumbnail">
+                <img src="http://placehold.it/500x250/EEE">
+                  <div class="caption">
+                    <h4>Thumbnail label</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, soluta, eligendi doloribus sunt minus amet sit debitis repellat. Consectetur, culpa itaque odio similique suscipit</p>
+                    <a href="#" class="btn btn-default btn-xs pull-right" role="button"><i class="glyphicon glyphicon-edit"></i></a> <a href="#" class="btn btn-info btn-xs" role="button">Button</a> <a href="#" class="btn btn-default btn-xs" role="button">Button</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-xs-18 col-sm-6 col-md-3">
+              <div class="thumbnail">
+                <img src="http://placehold.it/500x250/EEE">
+                  <div class="caption">
+                    <h4>Thumbnail label</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, soluta, eligendi doloribus sunt minus amet sit debitis repellat. Consectetur, culpa itaque odio similique suscipit</p>
+                    <a href="#" class="btn btn-default btn-xs pull-right" role="button"><i class="glyphicon glyphicon-edit"></i></a> <a href="#" class="btn btn-info btn-xs" role="button">Button</a> <a href="#" class="btn btn-default btn-xs" role="button">Button</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-xs-18 col-sm-6 col-md-3">
+              <div class="thumbnail">
+                <img src="http://placehold.it/500x250/EEE">
+                  <div class="caption">
+                    <h4>Thumbnail label</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, soluta, eligendi doloribus sunt minus amet sit debitis repellat. Consectetur, culpa itaque odio similique suscipit</p>
+                    <a href="#" class="btn btn-default btn-xs pull-right" role="button"><i class="glyphicon glyphicon-edit"></i></a> <a href="#" class="btn btn-info btn-xs" role="button">Button</a> <a href="#" class="btn btn-default btn-xs" role="button">Button</a>
+                </div>
+              </div>
+            </div>
+        </div><!--/row-->
+      </div><!--/container -->
           
             <br>
             <div class="container">
