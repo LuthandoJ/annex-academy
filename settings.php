6dccfa287cb15b1fb988762c59b6837b78137496
asithembiso <asithembiso@gmail.com>
2017-02-28 09:45:12 +0200

    navigation and active status

--- settings.php
+++ settings.php
@@ -25,7 +25,41 @@
 <body>
 
     <!-- Navigation -->
-
+    <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-top: -100px;">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">
+                    <img src="http://placehold.it/150x50&text=Logo" alt="">
+                </a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1" style="margin-right: -80px;">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="index.php"><i class="fa fa-dashboard" aria-hidden="true"></i> Dashboard</a></li>
+                    <li><a href="tutorials.php"><i class="fa fa-book" aria-hidden="true"></i> Tutorials</a></li>
+                    <li><a href="recources.php"><i class="fa fa-question-circle" aria-hidden="true"></i> Recources</a></li>
+                    <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="profile.php"><i class="fa fa-fw fa-user"></i> Profile</a></li>
+                        <li class="active"><a href="settings.php"><i class="fa fa-fw fa-gear"></i> Settings</a></li>
+                        <li class="divider"></li>
+                        <li><a href="logout.php"><i class="fa fa-fw fa-power-off"></i> Log Out</a></li>
+                    </ul>
+                </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
 
 
     <!-- Page Content -->
@@ -65,3 +99,4 @@
 </body>
 
 </html>
+
