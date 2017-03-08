03abbc91c74d5963eb3614d3b4aaa913602393a2
asithembiso <asithembiso@gmail.com>
2017-03-08 21:06:22 +0200

    removed echo con successful

--- assets/include/config.php
+++ assets/include/config.php
@@ -2,7 +2,6 @@
 	$connect = @new mysqli("localhost", "root", "Sit@0923", "bingo");
 
 	if(!$connect->connect_error) {
-	// echo "Successfully connected";
 	}
 	else {
 		die("Connection Failed : " . $connect->connect_error);
